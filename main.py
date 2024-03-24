2866598733b1f33a0c33c31b4a53ad38136929c7
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-03-24 11:16:29 +0100

    "elif"

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 height =  int(input("Enter your height in cm:\n"))
+limit_age = 12
 
 if height < 120:
     print(f"YOu are not allowed to enter.")
@@ -6,5 +7,7 @@ else:
     age =  int(input("Enter your age:\n"))
     if age >= 18:
         print(f"You need to pay 12$")
-    else:
+    elif age >= 12 and age <18:
         print(f"You need to pay 7$")
+    else:
+        print(f"You are smaller than {limit_age}. You need to pay 5$.")
