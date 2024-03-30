9eabf70b0fa251185d80855d8b630721581d7ac2
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-03-30 03:23:44 +0100

    "Random Module"

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print(16 - 2 * 5 // 3 + 1)
+import random
+import my_module
+
+# rand_int = random.randint(1, 10)
+# print(rand_int)
+
+print(my_module.pi)
+
+rand_float = random.random()
+
+print(rand_float)
+
+rand_decimal = random.random()
+
+rand_0_to_5 = round(5 * (rand_decimal))
+
+print(rand_0_to_5)
