955e1a88bcd680d160f99f9a267a00cfba14722e
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-02 03:22:07 +0200

    "Average Height"

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-fruits = ["apple", "peach", "banana"]
+student_heights = input().split()
+print(student_heights)
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
 
-for fruit in fruits:
-    print(fruit + " pie")
-    print(fruits)
+total_height = 0
+for height in student_heights:
+    total_height += height
+
+print(round(total_height / (len(student_heights))))
