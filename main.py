d049dd10d777e020d07484c34788c69f98323a5c
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-17 09:02:43 +0200

    "range()"

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
-import turtle
+for _ in range(3):
+    print("Hello")
+    print("Hello again!")
 
-screen = turtle.Screen()
-screen.bgcolor("lightgreen")
-alex = turtle.Turtle()
-alex.color("red")
-alex.pensize(3)
-
-tina = turtle.Turtle()
-tina.color("blue")
-tina.pensize(5)
-
-alex.forward(150)
-alex.left(90)
-alex.forward(200)
-
-tina.right(135)
-tina.forward(50)
-tina.right(75)
-tina.forward(80)
-
-screen.exitonclick()
+print("Hello finally!")
