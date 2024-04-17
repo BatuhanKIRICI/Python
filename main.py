e00f41773fe418bdddf517608c2058d397f0b38c
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-17 09:00:08 +0200

    "Colorized"

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
 import turtle
 
-wn = turtle.Screen()
+screen = turtle.Screen()
+screen.bgcolor("lightgreen")
 alex = turtle.Turtle()
+alex.color("red")
+alex.pensize(3)
+
+tina = turtle.Turtle()
+tina.color("blue")
+tina.pensize(5)
+
 alex.forward(150)
 alex.left(90)
 alex.forward(200)
 
-wn.exitonclick()
+tina.right(135)
+tina.forward(50)
+tina.right(75)
+tina.forward(80)
+
+screen.exitonclick()
