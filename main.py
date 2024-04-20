6649e0a021f48cf703a8882e30c2ebbe3c3a7cfb
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-20 13:16:26 +0200

    "More than one parameter"

--- main.py
+++ main.py
@@ -7,11 +7,18 @@
 # greet()
 
 
-def greet_by_name(name):
-    print(f"Hello {name}!")
-    print(f"Hi {name}!")
+# def greet_by_name(name):
+#     print(f"Hello {name}!")
+#     print(f"Hi {name}!")
 
 
-greet_by_name("Adam")
+# greet_by_name("Adam")
 
-greet_by_name("Alex")
+# greet_by_name("Alex")
+
+
+def greet_with_location(name, location):
+    print(f"I'm {name} from {location}.")
+
+
+greet_with_location("Alex", "Houston")
