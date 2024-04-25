2c627973fa6d076b9f53aed8836ad0a141377dfb
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-25 08:45:48 +0200

    "Decryption"

--- main.py
+++ main.py
@@ -68,4 +68,16 @@ def encrypt(plain_text, shift_amount):
     print(cipher_text)
 
 
-encrypt(plain_text=text, shift_amount=shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"{plain_text}")
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(cipher_text=text, shift_amount=shift)
