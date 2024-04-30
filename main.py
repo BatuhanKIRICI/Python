197005e76ca55a341d26984435b8791927652cc1
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-30 18:42:59 +0200

    "Dictionary for loop"

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ programming_dictionary = {
     "Loop": "The action of doing something over and over again.",
 }
 
-print(programming_dictionary["Bug"])
+# print(programming_dictionary["Bug"])
 
 programming_dictionary["New_Bug"] = "System failed!"
 
-print(programming_dictionary["New_Bug"])
+# print(programming_dictionary["New_Bug"])
 
 # Create a new dictionary
 empty_dictionary = {}
@@ -16,9 +16,13 @@ empty_dictionary = {}
 # Wiping an existing dictionary
 programming_dictionary = {}
 
-print(programming_dictionary)
+# print(programming_dictionary)
 
 # Edit an item in dictionary
 programming_dictionary["Bug"] = "A moth in your computer."
 
-print(programming_dictionary)
+# print(programming_dictionary)
+
+for thing in programming_dictionary:
+    print(thing)
+    print(programming_dictionary[thing])
