288341fef97ab86273b68774fe315a253b42a950
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-04-30 21:44:13 +0200

    "Drawing pentagon with turtle and for loop"

--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 import turtle
-import math
 
 wn = turtle.Screen()
 
 bob = turtle.Turtle()
 
-bob.right(90)
-bob.forward(50)
-bob.left(90)
-bob.forward(50)
-bob.left(90)
-bob.forward(50)
-bob.left(90)
-bob.forward(50)
-bob.right(135)
-
-distance = math.sqrt(50 * 50 / 2)
-
-bob.forward(distance)
-bob.right(90)
-bob.forward(distance)
-
-wn.exitonclick()
+for _ in range(5):
+    bob.color("red", "green")
+    bob.forward(50)
+    bob.right(72)
