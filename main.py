bf42bcf6f6930a5a8b5b3cfd260811dbec255454
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-04 20:19:52 +0200

    "Splitting and Joining"

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-list = ["apple", "dell", 5, []]
+song = "This state is elevating..."
+numbers = "1,2,3 4 5"
+number = str(1325651556)
 
-new_list = (list + ["65"]) * 3
+add = "\\"
+
+new_list = song.split()
+new_list_2 = numbers.split()
+new_list_3 = number.split()
+new_list_4 = song.split("is")
+new_list_5 = add.join(song)
+
 
 print(new_list)
+print(new_list_2)
+print(new_list_3)
+print(new_list_4)
+print(new_list_5)
