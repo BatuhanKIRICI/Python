3489034267d35c2ba33f93310755600fc1a9d912
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-05 23:04:55 +0200

    "range() function"

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+nums = range(0, 10)
+
+print(nums)
 
 accum = 0
 
-for x in nums:
-    accum += x
-
+for i in nums:
+    accum += i
 print(accum)
+
+print(list(range(5)))
+
+print(list(range(2, 7)))
