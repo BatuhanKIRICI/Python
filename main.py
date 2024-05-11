b4516761239642e30c8a682db0bd40c6d7a1fffe
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-11 23:42:06 +0200

    "Aliasing"

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-a = "banana"
+# a = "banana"
 
-b = "banan"
+# b = "banan"
 
-print(a is not b)
+# print(a is not b)
 
 list_1 = [75, 76, 77]
 list_2 = [75, 76, 77]
 
 print(list_1 == list_2)
+print(list_1 is list_2)
 
-print(id(list_1))
-print(id(list_2))
+list_1 = list_2
 
-print(id(a))
-print(id(b))
+print(list_1 is list_2)
+
+# print(id(list_1))
+# print(id(list_2))
+
+# print(id(a))
+# print(id(b))
