fcca12ca215801e88d9dc352b2d80b4f93506e6d
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-12 09:58:47 +0200

    "list methods"

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-alist = [4, 2, 8, 6, 5]
-blist = alist * 2
+my_list = []
 
-print(blist)
+my_list.append(5)
+my_list.append(25)
+my_list.append(60)
+my_list.append(60)
+my_list.append("60")
 
-blist[3] = 999
+# print(my_list)
+# print(my_list.index("60"))
 
-print(blist)
+my_list.insert(1, 133)
+
+# print(my_list)
+# print(my_list.count(60))
+
+# print(my_list.index("60"))
+
+my_list.reverse()
+
+# print(my_list)
+
+my_list.remove("60")
+
+my_list.sort()
+# my_list.reverse()
+
+print(my_list)
+
+new_item = my_list.pop()
+
+print(my_list, new_item)
