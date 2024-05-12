890ed4054f14d08038d01d7bb60d4e69d941cf36
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-12 17:42:59 +0200

    "return usage"

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
+# def my_name(f_name, l_name):
+#     print(f"{f_name.title()} {l_name.title()}")
+
+
+# my_name("baTUhan", "KIricI")
+
+
 def my_name(f_name, l_name):
-    print(f"{f_name.title()} {l_name.title()}")
+    return f"{f_name.title()} {l_name.title()}"
 
 
-my_name("batuhan", "kirici")
+formatted_name = my_name("baTUhan", "KIricI")
+
+print(formatted_name)
