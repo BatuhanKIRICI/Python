ba1ee84102c20b3811ac64c6d14c61699d7eacee
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-15 15:14:54 +0200

    "Calculator Version.1"

--- main.py
+++ main.py
@@ -1,59 +1,32 @@
-# scores = "67 80 90 78 93 20 79 89 96 97 92 88 79 68 58 90 98 100 79 74 83 88 80 86 85 70 90 100"
-# new_list = scores.split()
+def add(n1, n2):
+    return n1 + n2
 
-# # print(new_list)
 
-# a_scores = 0
+def subtract(n1, n2):
+    return n1 - n2
 
-# for score in new_list:
-#     if int(score) >= 90:
-#         a_scores += 1
 
-# # print(a_scores)
+def multiply(n1, n2):
+    return n1 * n2
 
-# stopwords = ["to", "a", "for", "by", "an", "am", "the", "so", "it", "and", "The"]
-# org = "The organization for health, safety, and education"
-# acro = ""
-# lst = org.split()
 
-# for i in lst:
-#     if i in stopwords:
-#         lst.remove(i)
+def divide(n1, n2):
+    return n1 / n2
 
-# for j in lst:
-#     acro += j[0]
 
-# acro = acro.upper()
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
-# stopwords = ["to", "a", "for", "by", "an", "am", "the", "so", "it", "and", "The"]
-# sent = "The water earth and air are vital"
-# acro = ""
-# lst = sent.split()
+num_1 = int(input("What is the first number?: "))
+num_2 = int(input("What is the second number?: "))
 
-# for i in lst:
-#     if i in stopwords:
-#         lst.remove(i)
-
-# for j in lst:
-#     acro = acro + j[0] + j[1]
-#     if j != lst[-1]:
-#         acro += ". "
-
-# acro = acro.upper()
-
-# p_phrase = "was it a car or a cat I saw"
-# r_phrase = p_phrase[::-1]
-
-# print(r_phrase)
-
-inventory = [
-    "shoes, 12, 29.99",
-    "shirts, 20, 9.99",
-    "sweatpants, 25, 15.00",
-    "scarves, 13, 7.75",
-]
-for temp in inventory:
-    temp = temp.split(",")
-    str1 = "The store has{} {}, each for{} USD."
-    str1 = str1.format(temp[1], temp[0], temp[2])
-    print(str1)
+for item in operations:
+    operation_symbol = input("Select an operation symbol: ")
+    if operation_symbol == "+":
+        print(f"{num_1} {operation_symbol} {num_2} is {add(num_1, num_2)}.")
+    elif operation_symbol == "-":
+        print(f"{num_1} {operation_symbol} {num_2} is {subtract(num_1, num_2)}.")
+    elif operation_symbol == "*":
+        print(f"{num_1} {operation_symbol} {num_2} is {multiply(num_1, num_2)}.")
+    elif operation_symbol == "/":
+        print(f"{num_1} {operation_symbol} {num_2} is {divide(num_1, num_2)}.")
+    break
