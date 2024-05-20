121cda6e2ce292d6140bf8d2b18578808075f276
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-20 22:07:41 +0200

    "Trial for git push problem"

--- main.py
+++ main.py
@@ -1,37 +1,38 @@
-def add(n1, n2):
-    return n1 + n2
+# def add(n1, n2):
+#     return n1 + n2
 
 
-def subtract(n1, n2):
-    return n1 - n2
+# def subtract(n1, n2):
+#     return n1 - n2
 
 
-def multiply(n1, n2):
-    return n1 * n2
+# def multiply(n1, n2):
+#     return n1 * n2
 
 
-def divide(n1, n2):
-    return n1 / n2
+# def divide(n1, n2):
+#     return n1 / n2
 
 
-operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+# operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
-num_1 = int(input("What is the first number?: "))
-num_2 = int(input("What is the second number?: "))
+# num_1 = int(input("What is the first number?: "))
+# num_2 = int(input("What is the second number?: "))
 
-for item in operations:
-    operation_symbol = input("Select an operation symbol: ")
-    # if operation_symbol == "+":
-    #     print(f"{num_1} {operation_symbol} {num_2} is {add(num_1, num_2)}.")
-    # elif operation_symbol == "-":
-    #     print(f"{num_1} {operation_symbol} {num_2} is {subtract(num_1, num_2)}.")
-    # elif operation_symbol == "*":
-    #     print(f"{num_1} {operation_symbol} {num_2} is {multiply(num_1, num_2)}.")
-    # elif operation_symbol == "/":
-    #     print(f"{num_1} {operation_symbol} {num_2} is {divide(num_1, num_2)}.")
-    # break
-    calculation_function = operations[operation_symbol]
-    print(
-        f"{num_1} {operation_symbol} {num_2} is {calculation_function(num_1, num_2)}."
-    )
-    break
+# for item in operations:
+#     operation_symbol = input("Select an operation symbol: ")
+#     # if operation_symbol == "+":
+#     #     print(f"{num_1} {operation_symbol} {num_2} is {add(num_1, num_2)}.")
+#     # elif operation_symbol == "-":
+#     #     print(f"{num_1} {operation_symbol} {num_2} is {subtract(num_1, num_2)}.")
+#     # elif operation_symbol == "*":
+#     #     print(f"{num_1} {operation_symbol} {num_2} is {multiply(num_1, num_2)}.")
+#     # elif operation_symbol == "/":
+#     #     print(f"{num_1} {operation_symbol} {num_2} is {divide(num_1, num_2)}.")
+#     # break
+#     calculation_function = operations[operation_symbol]
+#     print(
+#         f"{num_1} {operation_symbol} {num_2} is {calculation_function(num_1, num_2)}."
+#     )
+#     break
+print("Trial for git push problem")
