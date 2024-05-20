fa3d60b56c973641d40eb9f1371c3d40010c4037
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-20 22:41:21 +0200

    "Calculator"

--- main.py
+++ main.py
@@ -1,38 +1,46 @@
-# def add(n1, n2):
-#     return n1 + n2
+def add(n1, n2):
+    return n1 + n2
 
 
-# def subtract(n1, n2):
-#     return n1 - n2
+def subtract(n1, n2):
+    return n1 - n2
 
 
-# def multiply(n1, n2):
-#     return n1 * n2
+def multiply(n1, n2):
+    return n1 * n2
 
 
-# def divide(n1, n2):
-#     return n1 / n2
+def divide(n1, n2):
+    return n1 / n2
 
 
-# operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
+operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
 
-# num_1 = int(input("What is the first number?: "))
-# num_2 = int(input("What is the second number?: "))
 
-# for item in operations:
-#     operation_symbol = input("Select an operation symbol: ")
-#     # if operation_symbol == "+":
-#     #     print(f"{num_1} {operation_symbol} {num_2} is {add(num_1, num_2)}.")
-#     # elif operation_symbol == "-":
-#     #     print(f"{num_1} {operation_symbol} {num_2} is {subtract(num_1, num_2)}.")
-#     # elif operation_symbol == "*":
-#     #     print(f"{num_1} {operation_symbol} {num_2} is {multiply(num_1, num_2)}.")
-#     # elif operation_symbol == "/":
-#     #     print(f"{num_1} {operation_symbol} {num_2} is {divide(num_1, num_2)}.")
-#     # break
-#     calculation_function = operations[operation_symbol]
-#     print(
-#         f"{num_1} {operation_symbol} {num_2} is {calculation_function(num_1, num_2)}."
-#     )
-#     break
-print("Trial for git push problem")
+def calculator():
+    num1 = float(input("+"))
+    for symbol in operations:
+        print(symbol)
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation: ")
+        num2 = float(input("What's the next number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+    if (
+        input(
+            f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
+        )
+        == "y"
+    ):
+        num1 = answer
+    else:
+        should_continue = False
+
+        calculator()
+
+
+calculator()
