3ffc09c5353ac134c1649b6f13e1429e4a348357
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-21 21:55:11 +0200

    "Blackjack"

--- main.py
+++ main.py
@@ -1,46 +1,81 @@
-def add(n1, n2):
-    return n1 + n2
+import random
 
 
-def subtract(n1, n2):
-    return n1 - n2
+def deal_card():
+
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    card = random.choice(cards)
+    return card
 
 
-def multiply(n1, n2):
-    return n1 * n2
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+    return sum(cards)
 
 
-def divide(n1, n2):
-    return n1 / n2
+def compare(user_score, computer_score):
 
+    if user_score > 21 and computer_score > 21:
+        return "You went over. You lose 😤"
 
-operations = {"+": add, "-": subtract, "*": multiply, "/": divide}
-
-
-def calculator():
-    num1 = float(input("+"))
-    for symbol in operations:
-        print(symbol)
-    should_continue = True
-
-    while should_continue:
-        operation_symbol = input("Pick an operation: ")
-        num2 = float(input("What's the next number?: "))
-        calculation_function = operations[operation_symbol]
-        answer = calculation_function(num1, num2)
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
-
-    if (
-        input(
-            f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: "
-        )
-        == "y"
-    ):
-        num1 = answer
+    if user_score == computer_score:
+        return "Draw 🙃"
+    elif computer_score == 0:
+        return "Lose, opponent has Blackjack 😱"
+    elif user_score == 0:
+        return "Win with a Blackjack 😎"
+    elif user_score > 21:
+        return "You went over. You lose 😭"
+    elif computer_score > 21:
+        return "Opponent went over. You win 😁"
+    elif user_score > computer_score:
+        return "You win 😃"
     else:
-        should_continue = False
-
-        calculator()
+        return "You lose 😤"
 
 
-calculator()
+def play_game():
+
+    user_cards = []
+    computer_cards = []
+    is_game_over = False
+
+    for _ in range(2):
+        user_cards.append(deal_card())
+        computer_cards.append(deal_card())
+
+    while not is_game_over:
+
+        user_score = calculate_score(user_cards)
+        computer_score = calculate_score(computer_cards)
+        print(f"   Your cards: {user_cards}, current score: {user_score}")
+        print(f"   Computer's first card: {computer_cards[0]}")
+
+        if user_score == 0 or computer_score == 0 or user_score > 21:
+            is_game_over = True
+        else:
+
+            user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+            if user_should_deal == "y":
+                user_cards.append(deal_card())
+            else:
+                is_game_over = True
+
+    while computer_score != 0 and computer_score < 17:
+        computer_cards.append(deal_card())
+        computer_score = calculate_score(computer_cards)
+
+    print(f"   Your final hand: {user_cards}, final score: {user_score}")
+    print(f"   Computer's final hand: {computer_cards}, final score: {computer_score}")
+    print(compare(user_score, computer_score))
+
+
+while input("Do you want to play a game of Blackjack? Type 'y' or 'n': ") == "y":
+
+    play_game()
