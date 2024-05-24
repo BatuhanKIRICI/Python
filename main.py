3f4f6b9244f8390ed3f68b67f5727c7f48af94d8
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-24 09:55:21 +0200

    "Number guessing game"

--- main.py
+++ main.py
@@ -1,11 +1,71 @@
-enemies = 3
+import random
+
+computer_number = random.randrange(1, 100)
+
+difficulty = input("Select difficulty! Type 'e' for easy, type 'h' for hard. ").lower()
+
+count = 10
+
+list_higher = [100]
+list_lower = [0]
+
+game_is_on = True
 
 
-def increase_enemies():
-    global enemies
-    enemies += 1
-    print(f"Number of enemies is {enemies}.")
+while game_is_on:
 
+    guessed_num = int(input("Guess a number: "))
 
-increase_enemies()
-print(f"Number of enemies is {enemies}.")
+    if difficulty == "h":
+        if guessed_num > computer_number and count > 6:
+
+            list_higher.append(guessed_num)
+            count -= 1
+            print(
+                f"Too high! Number between {max(list_lower)} and {min(list_higher)}. And you have {count-5} shot to try!"
+            )
+
+        elif guessed_num < computer_number and count > 6:
+
+            list_lower.append(guessed_num)
+            count -= 1
+            if (count - 5) > 0:
+                print(
+                    f"Too low! Number between {max(list_lower)} and {min(list_higher)}. And you have {count-5} shot to try!"
+                )
+            else:
+                print(f"You've got no shot!")
+
+        elif guessed_num == computer_number and count > 6:
+            print(f"Congratulations!!!")
+            break
+        else:
+            print(f"You've lost the game!")
+            break
+
+    elif difficulty == "e":
+        if guessed_num > computer_number and count > 1:
+
+            list_higher.append(guessed_num)
+            count -= 1
+            print(
+                f"Too high! Number between {max(list_lower)} and {min(list_higher)}. And you have {count} shot to try!"
+            )
+
+        elif guessed_num < computer_number and count > 1:
+
+            list_lower.append(guessed_num)
+            count -= 1
+            if (count) > 0:
+                print(
+                    f"Too low! Number between {max(list_lower)} and {min(list_higher)}. And you have {count} shot to try!"
+                )
+            else:
+                print(f"You've got no shot!")
+
+        elif guessed_num == computer_number and count > 1:
+            print(f"Congratulations!!!")
+            break
+        else:
+            print(f"You've lost the game!")
+            break
