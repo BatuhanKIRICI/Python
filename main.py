d7b1c885a8e266db9d4b085921f9cbcedf7276b6
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-05-27 15:41:20 +0200

    "Debugging"

--- main.py
+++ main.py
@@ -1,139 +1,30 @@
-import random
+# def my_function():
+#     for i in range(1, 20):
+#         if i == 20:
+#             print("You got it!")
 
 
-def game_play():
-    game_is_on = True
-    computer_number = random.randrange(1, 100)
+# my_function()
 
-    difficulty = input(
-        "Select difficulty! Type 'e' for easy, type 'h' for hard. "
-    ).lower()
+# pages = 0
 
-    count = 10
+# words_per_page = 0
 
-    list_higher = [100]
-    list_lower = [0]
+# pages = int(input("Enter number of pages: "))
 
-    while game_is_on:
-        guessed_num = int(input("Guess a number: "))
+# words_per_page == int(input("Enter number of words: "))
 
-        if difficulty == "h":
-            if guessed_num > computer_number and count > 6:
+# total_words = pages * words_per_page
 
-                list_higher.append(guessed_num)
-                count -= 1
-
-                result = f"Too high! Number between {max(list_lower)} and {min(list_higher)}. And you have {count-5} shot to try!"
-
-                print(f"{result}")
-
-            elif guessed_num < computer_number and count > 6:
-
-                list_lower.append(guessed_num)
-                count -= 1
-
-                result = f"Too low! Number between {max(list_lower)} and {min(list_higher)}. And you have {count-5} shot to try!"
-
-                if (count - 5) > 0:
-                    print(f"{result}")
-
-            elif guessed_num == computer_number and count > 6:
-
-                result = f"Congratulations!!!"
-
-                print(f"{result}")
-                play_again = (
-                    input("Do you want to play again? (yes/no): ").strip().lower()
-                )
-                if play_again == "y":
-                    difficulty = input(
-                        "Select difficulty! Type 'e' for easy, type 'h' for hard. "
-                    ).lower()
-                    computer_number = random.randrange(1, 100)
-                    count = 10
-
-                    list_higher = [100]
-                    list_lower = [0]
-                    continue
-                else:
-                    print("Goodbye!")
-                    break
-
-            else:
-                result = f"You've got no shot! You've lost the game! The number was {computer_number}."
-                print(f"{result}")
-                play_again = (
-                    input("Do you want to play again? (yes/no): ").strip().lower()
-                )
-                if play_again == "y":
-                    difficulty = input(
-                        "Select difficulty! Type 'e' for easy, type 'h' for hard. "
-                    ).lower()
-                    computer_number = random.randrange(1, 100)
-                    count = 10
-
-                    list_higher = [100]
-                    list_lower = [0]
-                    continue
-                else:
-                    print("Goodbye!")
-                    break
-
-        elif difficulty == "e":
-            if guessed_num > computer_number and count > 1:
-
-                list_higher.append(guessed_num)
-                count -= 1
-                result = f"Too high! Number between {max(list_lower)} and {min(list_higher)}. And you have {count} shot to try!"
-                print(f"{result}")
-
-            elif guessed_num < computer_number and count > 1:
-
-                list_lower.append(guessed_num)
-                count -= 1
-                result = f"Too low! Number between {max(list_lower)} and {min(list_higher)}. And you have {count} shot to try!"
-                if (count) > 0:
-                    print(f"{result}")
-
-            elif guessed_num == computer_number and count > 1:
-                result = f"Congratulations!!!"
-                print(f"{result}")
-                play_again = (
-                    input("Do you want to play again? (yes/no): ").strip().lower()
-                )
-                if play_again == "y":
-                    difficulty = input(
-                        "Select difficulty! Type 'e' for easy, type 'h' for hard. "
-                    ).lower()
-                    computer_number = random.randrange(1, 100)
-                    count = 10
-
-                    list_higher = [100]
-                    list_lower = [0]
-                    continue
-                else:
-                    print("Goodbye!")
-                    break
-
-            else:
-                result = f"You've got no shot! You've lost the game! The number was {computer_number}."
-                print(f"{result}")
-                play_again = (
-                    input("Do you want to play again? (yes/no): ").strip().lower()
-                )
-                if play_again == "y":
-                    difficulty = input(
-                        "Select difficulty! Type 'e' for easy, type 'h' for hard. "
-                    ).lower()
-                    computer_number = random.randrange(1, 100)
-                    count = 10
-
-                    list_higher = [100]
-                    list_lower = [0]
-                    continue
-                else:
-                    print("Goodbye!")
-                    break
+# print(total_words)
 
 
-game_play()
+def mutate_list(a_list):
+    b_list = []
+    for item in a_list:
+        new_item = item * 2
+        b_list.append(new_item)
+    print(b_list)
+
+
+mutate_list([1, 23, 45])
