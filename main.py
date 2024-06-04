7427f42aac4624d00dce31a894bc4c44fd2901dd
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-04 12:53:27 +0200

    "Coffee machine initialization"

--- main.py
+++ main.py
@@ -1,30 +1,82 @@
-# def my_function():
-#     for i in range(1, 20):
-#         if i == 20:
-#             print("You got it!")
+from list import MENU, resources
+import math
+
+turn_on = True
+
+penny = 0.01
+nickel = 0.05
+dime = 0.10
+quarter = 0.25
+
+espresso_price = 1.5
+latte_price = 2.5
+cappucino_price = 2.5
+
+money = 0
+
+choice_of_customer = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
 
-# my_function()
+if choice_of_customer == "report":
+    for item in resources:
+        quantity = resources[item]
+        if item == "water" or item == "milk":
+            print(f"{item.title()}: {quantity}ml")
+        else:
+            print(f"{item.title()}: {quantity}g")
+            print(f"Money: ${money}")
+elif choice_of_customer == "espresso":
+    print("Please insert coins!")
+    penny_quantity = int(input("How many pennies? "))
+    nickel_quantity = int(input("How many nickels? "))
+    dime_quantity = int(input("How many dimes? "))
+    quarter_quantity = int(input("How many quarters? "))
 
-# pages = 0
+    diff = round(
+        penny * penny_quantity
+        + nickel * nickel_quantity
+        + dime * dime_quantity
+        + quarter * quarter_quantity
+        - espresso_price,
+        2,
+    )
 
-# words_per_page = 0
+    print(f"${diff}")
 
-# pages = int(input("Enter number of pages: "))
+elif choice_of_customer == "latte":
+    print("Please insert coins!")
+    penny_quantity = int(input("How many pennies? "))
+    nickel_quantity = int(input("How many nickels? "))
+    dime_quantity = int(input("How many dimes? "))
+    quarter_quantity = int(input("How many quarters? "))
 
-# words_per_page == int(input("Enter number of words: "))
+    diff = round(
+        penny * penny_quantity
+        + nickel * nickel_quantity
+        + dime * dime_quantity
+        + quarter * quarter_quantity
+        - latte_price,
+        2,
+    )
 
-# total_words = pages * words_per_page
+    print(f"${diff}")
 
-# print(total_words)
+elif choice_of_customer == "cappucino":
+    print("Please insert coins!")
+    penny_quantity = int(input("How many pennies? "))
+    nickel_quantity = int(input("How many nickels? "))
+    dime_quantity = int(input("How many dimes? "))
+    quarter_quantity = int(input("How many quarters? "))
 
+    diff = round(
+        penny * penny_quantity
+        + nickel * nickel_quantity
+        + dime * dime_quantity
+        + quarter * quarter_quantity
+        - cappucino_price,
+        2,
+    )
 
-def mutate_list(a_list):
-    b_list = []
-    for item in a_list:
-        new_item = item * 2
-        b_list.append(new_item)
-    print(b_list)
+    print(f"${diff}")
 
-
-mutate_list([1, 23, 45])
+# print(choice_of_customer)
