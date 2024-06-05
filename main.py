2285d6af593a27798f15bc7e9c3284ee1107fb46
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-05 10:04:59 +0200

    "While loop for coffe machine"

--- main.py
+++ main.py
@@ -1,82 +1,114 @@
 from list import MENU, resources
 import math
 
-turn_on = True
 
-penny = 0.01
-nickel = 0.05
-dime = 0.10
-quarter = 0.25
+def coffee_machine():
 
-espresso_price = 1.5
-latte_price = 2.5
-cappucino_price = 2.5
+    turn_on = False
 
-money = 0
+    penny = 0.01
+    nickel = 0.05
+    dime = 0.10
+    quarter = 0.25
 
-choice_of_customer = input("What would you like? (espresso/latte/cappuccino): ").lower()
+    espresso_price = 1.5
+    latte_price = 2.5
+    cappucino_price = 2.5
 
+    money = 100.00
 
-if choice_of_customer == "report":
-    for item in resources:
-        quantity = resources[item]
-        if item == "water" or item == "milk":
-            print(f"{item.title()}: {quantity}ml")
+    machine = input("Turn on the machine? (yes/no): ").lower()
+
+    if machine == "yes":
+        turn_on = True
+    elif machine == "no":
+        turn_on = False
+
+    while turn_on == True:
+
+        choice_of_customer = input(
+            """What would you like? (espresso/latte/cappuccino or "off" to turn off the machine") """
+        ).lower()
+
+        if choice_of_customer == "report":
+            for item in resources:
+                quantity = resources[item]
+                if item == "water" or item == "milk":
+                    print(f"{item.title()}: {quantity}ml")
+                else:
+                    print(f"{item.title()}: {quantity}g")
+                    print(f"Money: ${money}")
+        elif choice_of_customer == "espresso":
+            print("Please insert coins!")
+            penny_quantity = int(input("How many pennies? "))
+            nickel_quantity = int(input("How many nickels? "))
+            dime_quantity = int(input("How many dimes? "))
+            quarter_quantity = int(input("How many quarters? "))
+
+            resources["water"] -= MENU["espresso"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["espresso"]["ingredients"]["coffee"]
+
+            money += MENU["espresso"]["cost"]
+
+            diff = round(
+                penny * penny_quantity
+                + nickel * nickel_quantity
+                + dime * dime_quantity
+                + quarter * quarter_quantity
+                - espresso_price,
+                2,
+            )
+
+        elif choice_of_customer == "latte":
+            print("Please insert coins!")
+            penny_quantity = int(input("How many pennies? "))
+            nickel_quantity = int(input("How many nickels? "))
+            dime_quantity = int(input("How many dimes? "))
+            quarter_quantity = int(input("How many quarters? "))
+
+            resources["water"] -= MENU["latte"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["latte"]["ingredients"]["coffee"]
+            resources["milk"] -= MENU["latte"]["ingredients"]["milk"]
+
+            money += MENU["latte"]["cost"]
+
+            diff = round(
+                penny * penny_quantity
+                + nickel * nickel_quantity
+                + dime * dime_quantity
+                + quarter * quarter_quantity
+                - latte_price,
+                2,
+            )
+
+        elif choice_of_customer == "cappuccino":
+            print("Please insert coins!")
+            penny_quantity = int(input("How many pennies? "))
+            nickel_quantity = int(input("How many nickels? "))
+            dime_quantity = int(input("How many dimes? "))
+            quarter_quantity = int(input("How many quarters? "))
+
+            resources["water"] -= MENU["cappuccino"]["ingredients"]["water"]
+            resources["coffee"] -= MENU["cappuccino"]["ingredients"]["coffee"]
+            resources["milk"] -= MENU["cappuccino"]["ingredients"]["milk"]
+
+            money += MENU["cappuccino"]["cost"]
+
+            diff = round(
+                penny * penny_quantity
+                + nickel * nickel_quantity
+                + dime * dime_quantity
+                + quarter * quarter_quantity
+                - cappucino_price,
+                2,
+            )
+        elif choice_of_customer == "off":
+            break
+
+        if diff >= 0:
+            print(f"Here is ${diff} dollars in change. Preparing coffee...")
         else:
-            print(f"{item.title()}: {quantity}g")
-            print(f"Money: ${money}")
-elif choice_of_customer == "espresso":
-    print("Please insert coins!")
-    penny_quantity = int(input("How many pennies? "))
-    nickel_quantity = int(input("How many nickels? "))
-    dime_quantity = int(input("How many dimes? "))
-    quarter_quantity = int(input("How many quarters? "))
+            print("Sorry that's not enough money. Money refunded.")
 
-    diff = round(
-        penny * penny_quantity
-        + nickel * nickel_quantity
-        + dime * dime_quantity
-        + quarter * quarter_quantity
-        - espresso_price,
-        2,
-    )
 
-    print(f"${diff}")
-
-elif choice_of_customer == "latte":
-    print("Please insert coins!")
-    penny_quantity = int(input("How many pennies? "))
-    nickel_quantity = int(input("How many nickels? "))
-    dime_quantity = int(input("How many dimes? "))
-    quarter_quantity = int(input("How many quarters? "))
-
-    diff = round(
-        penny * penny_quantity
-        + nickel * nickel_quantity
-        + dime * dime_quantity
-        + quarter * quarter_quantity
-        - latte_price,
-        2,
-    )
-
-    print(f"${diff}")
-
-elif choice_of_customer == "cappucino":
-    print("Please insert coins!")
-    penny_quantity = int(input("How many pennies? "))
-    nickel_quantity = int(input("How many nickels? "))
-    dime_quantity = int(input("How many dimes? "))
-    quarter_quantity = int(input("How many quarters? "))
-
-    diff = round(
-        penny * penny_quantity
-        + nickel * nickel_quantity
-        + dime * dime_quantity
-        + quarter * quarter_quantity
-        - cappucino_price,
-        2,
-    )
-
-    print(f"${diff}")
-
-# print(choice_of_customer)
+coffee_machine()
