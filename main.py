009f1ac17092e3a9052b6e3a96670e2a60281bd6
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-09 12:34:33 +0200

    "Table alignment"

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-# from turtle import Turtle, Screen
-
-# timmy = Turtle()
-# print(timmy)
-
-# timmy.shape("turtle")
-# timmy.color("blue")
-
-# my_screen = Screen()
-
-# timmy.forward(150)
-
-# print(my_screen.canvheight)
-
-
-# my_screen.exitonclick()
-
 from prettytable import PrettyTable
 
 table = PrettyTable()
 
 table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
 
-print(table)
+# print(table)
 
 table.add_column("Type", ["Electric", "Water", "Fire"])
+table.align = "l"
 
 print(table)
