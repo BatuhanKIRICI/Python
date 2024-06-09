3b02f16bf101c527600c1e6dcceb28f5cfcb3db6
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-06-09 12:20:43 +0200

    "PrettyTable"

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
 
-timmy = Turtle()
-print(timmy)
+# timmy = Turtle()
+# print(timmy)
 
-timmy.shape("turtle")
-timmy.color("blue")
+# timmy.shape("turtle")
+# timmy.color("blue")
 
-my_screen = Screen()
+# my_screen = Screen()
 
-timmy.forward(150)
+# timmy.forward(150)
 
-print(my_screen.canvheight)
+# print(my_screen.canvheight)
 
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+
+print(table)
+
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)
