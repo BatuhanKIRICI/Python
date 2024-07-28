23469240de07e8278db2660730d00e326357b31b
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-28 22:09:15 +0200

    "del"

--- main.py
+++ main.py
@@ -1,13 +1,7 @@
-from data import question_data
-from question_model import Question
+hello = "hello"
 
+print(hello)
 
-question_bank = []
+del hello
 
-for item in question_data:
-    question_text = item["text"]
-    question_answer = item["answer"]
-    new_question = Question(q_text=question_text, q_answer=question_answer)
-    question_bank.append(new_question)
-
-print(question_bank[0])
+print(hello)
