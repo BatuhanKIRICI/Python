bc8a69236eb7680c9b4ceddb69f9937d9c6d2383
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-07-30 10:04:21 +0200

    "Match statement"

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-hello = "hello"
+http_status = 550
 
-print(hello)
+if http_status == 200 or http_status == 201:
+    print("Success")
+elif http_status == 400:
+    print("Bad request")
+elif http_status == 404:
+    print("Not found")
+elif http_status == 500 or http_status == 501:
+    print("Server error")
+else:
+    print("Unknown")
 
-del hello
-
-print(hello)
+match http_status:
+    case 200 | 201:
+        print("Success")
+    case _:
+        print("Unknown")
