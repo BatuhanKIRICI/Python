230206381405850d6705c560e25d821d6c1b01b6
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-03 11:09:41 +0200

    "args && kwargs"

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-set_a = {1, 2, 34, 5, 4}
-set_b = {3, 4, 5, 6, 7, 89}
+def sum(*args):
+    sum = 0
+    for x in args:
+        sum += x
+    return sum
 
-print(set_a.symmetric_difference(set_b))
-print(set_a ^ set_b)
+
+print(sum(4, 9, 8, 56))
+
+
+def sum(**kwargs):
+    sum = 0
+    for k, v in kwargs.items():
+        sum += v
+    return round(sum, 2)
+
+
+print(sum(coffee=3.00, tea=2.25, cake=5.99))
