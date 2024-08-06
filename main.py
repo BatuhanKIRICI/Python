4f9608856f136ec1cdc519f611cf74d800b09cc2
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-08-06 12:05:43 +0200

    "class example"

--- main.py
+++ main.py
@@ -1,19 +1,11 @@
-menu = ["espresso", "mocha", "latte", "americano", "cappuccino", "cortado"]
+class MyFirstClass:
+    print("Who wrote this?")
+    index = "Author-Book"
+
+    def hand_list(self, philosopher, book):
+        print(MyFirstClass.index)
+        print(philosopher + " wrote the book: " + book)
 
 
-def find_coffee(coffee):
-    if coffee[0] == "c":
-        return coffee
-
-
-map_coffee = map(find_coffee, menu)
-
-# print(map_coffee)
-
-# for item in map_coffee:
-#     print(item)
-
-filter_coffee = filter(find_coffee, menu)
-
-for x in filter_coffee:
-    print(x)
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
