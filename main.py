ebb092e2bbada6c0cd1a1d45458fb2dfe238f832
BatuhanKIRICI <batuhankirici01@gmail.com>
2024-09-15 09:39:50 +0200

    "Quiz"

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-class User:
-    def __init__(self, user_id, user_name):
-        self.id = user_id
-        self.username = user_name
-        self.followers = 0
-        self.following = 0
+from question_model import Question
+from data import question_data
+from quiz_brain import Quiz_Brain
 
-    def follow(self, user):
-        user.followers += 1
-        self.following += 1
+question_bank = []
 
+for question in question_data:
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-user_1 = User("001", "Michael")
-user_2 = User("002", "Laura")
-
-user_1.follow(user_2)
-
-print(user_1.followers)
-print(user_1.following)
-print(user_2.followers)
-print(user_2.following)
+quiz = Quiz_Brain(question_bank)
+quiz.next_question()
